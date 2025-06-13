b08c0a47f557692a80feb350b9b64d7f677ef669
Asghar Hussain <asghus117@gmail.com>
2025-06-13 22:08:11 +0530

    dont know

--- frontend/src/context/SoketContext.jsx
+++ frontend/src/context/SoketContext.jsx
@@ -8,7 +8,7 @@ export const SocketProvider = ({ children }) => {
   const [socket, setSocket] = useState(null);
 
   useEffect(() => {
-    const newSocket = io(import.meta.env.VITE_API_BASE_URL, {
+    const newSocket = io(import.meta.env.VITE_SOCKET_URL, {
       withCredentials: true, // ✅ This tells the browser to send cookies
     });
 
