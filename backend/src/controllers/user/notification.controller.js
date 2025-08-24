4d951d939cff1ae04053a75210852dfe61b6eb6f
Asghar Hussain <asghus117@gmail.com>
2025-08-25 01:47:40 +0530

    fix

--- backend/src/controllers/user/notification.controller.js
+++ backend/src/controllers/user/notification.controller.js
@@ -1,6 +1,6 @@
-import { ApiError } from "../../utils/ApiError";
-import { ApiResponse } from "../../utils/ApiResponse";
-import { asyncHandler } from "../../utils/asyncHandler";
+import { ApiError } from "../../utils/ApiError.js";
+import { ApiResponse } from "../../utils/ApiResponse.js";
+import { asyncHandler } from "../../utils/asyncHandler.js";
 
 export const getAllNotifications = asyncHandler(async(req,res,next)=>{
     const notifications = await Notification.find({ toUserId: req.userId })
